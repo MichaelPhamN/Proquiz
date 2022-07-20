a0d92d8f66fd6d21176b62409a3b2fd012bd184e
michaelphamn <michaelphamn@gmail.com>
2022-07-20 00:24:34 -0500

    Quiz Application Initial [JSP, JDBC, MySQL]

---
+++ src/main/java/com/example/proquiz/ProquizApplication.java
@@ -0,0 +1,13 @@
+package com.example.proquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProquizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProquizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proquiz/ProquizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProquizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
